d5364d8484dc9c8175d56dbf8b3885fb02b2a7e1
Marc Pekilidi <mpk@process-factory.dk>
2024-01-12 10:30:20 +0100

    WIP

---
+++ bin/user-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Prompt the user to enter a username
+echo "Please enter a username:"
+read username
+
+# Use the id command to check if the user exists
+if id "$username" &>/dev/null; then
+    echo "User $username exists, skipping"
+    exit 0
+else
+    echo "Creating new user: $username."
+    useradd -m -G sudo -s /bin/bash "$username"
+    passwd "$username"
+    tee /etc/wsl.conf << _EOF
+    [user]
+    default=${username}
+_EOF
+fi
